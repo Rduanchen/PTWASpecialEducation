3ea357c81a5c43fcd9a85f16ea92e5a4b08e8f3a
RayUTwolf <www.lovelion@gmail.com>
2024-10-11 15:18:39 +0800

    feat: distance calculator.

---
+++ src/utilitys/canvasTools.js
@@ -0,0 +1,6 @@
+export function centerLocator(object){
+    return {x: object.x - object.width/2, y: object.y - object.height/2}
+}
+export function distance(object_1, object_2){
+    return Math.pow(Math.pow(object_1.x-object_2.x,2)+Math.pow(object_1.y-object_2.y,2),0.5) 
+}
--- src/utilitys/centerLocator.js
+++
@@ -1,3 +0,0 @@
-export function centerLocator(object){
-    return {x: object.x - object.width/2, y: object.y - object.height/2}
-}
