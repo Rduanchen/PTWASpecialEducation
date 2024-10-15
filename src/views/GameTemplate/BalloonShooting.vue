1e9ce32220fd24125b2a6cf777e7fc5c4a0ffb33
RayUTwolf <www.lovelion@gmail.com>
2024-10-15 15:02:03 +0800

    feat: finish BalloonShooting game.

--- src/views/GameTemplate/BalloonShooting.vue
+++ src/views/GameTemplate/BalloonShooting.vue
@@ -2,7 +2,15 @@
   <div class="gameContainer">
     <div id="canvasContainer">
       <h2>{{ GameData.Question }}</h2>
-      <v-stage :config="configKonva">
+      <v-stage
+        :config="configKonva"
+        @mousedown="aimStart"
+        @mousemove="aimMove"
+        @mouseup="shoot"
+        @touchstart="aimStart"
+        @touchmove="aimMove"
+        @touchend="shoot"
+      >
         <v-layer>
           <v-rect :config="configBG"></v-rect>
         </v-layer>
@@ -13,6 +21,14 @@
             :config="balloon"
           ></v-circle>
           <v-text v-for="option in configOptions" :config="option"></v-text>
+        </v-layer>
+        <v-layer>
+          <v-circle v-if="isAiming" :config="configScope"></v-circle>
+          <v-line
+            v-if="isAiming"
+            v-for="line in configCross"
+            :config="line"
+          ></v-line>
         </v-layer>
       </v-stage>
     </div>
@@ -34,12 +50,24 @@ export default {
         fill: "gray",
         stroke: "gray",
       },
+      configScope: {
+        stroke: "red",
+      },
+      configCross: [
+        {
+          stroke: "red",
+        },
+        {
+          stroke: "red",
+        },
+      ],
 
       configBalloon: [],
       configOptions: [],
       balloonTemplate: ["pink", "lightblue", "yellow"],
 
       speed: 2,
+      isAiming: false,
 
       allOptions: [],
       trueOptions: [],
@@ -78,6 +106,7 @@ export default {
       this.configKonva.height = this.gameWidth / 2;
       this.configBG.width = this.gameWidth;
       this.configBG.height = this.gameWidth / 2;
+      this.configScope.radius = this.gameWidth * 0.08;
     },
     initializeOptions() {
       this.allOptions = this.GameData.True.concat(this.GameData.False);
@@ -90,7 +119,7 @@ export default {
     balloonSpawner() {
       let balloonType = Math.floor(Math.random() * this.balloonTemplate.length);
       let balloon = {};
-      balloon.radius = this.gameWidth * 0.075;
+      balloon.radius = this.gameWidth * 0.07;
       balloon.y = this.gameWidth * 0.5 + balloon.radius;
       balloon.x =
         Math.random() * (this.gameWidth - balloon.radius * 2) + balloon.radius;
@@ -117,6 +146,61 @@ export default {
           this.configOptions.splice(i, 1);
         }
       }
+    },
+    aimStart() {
+      this.isAiming = true;
+      clearInterval(this.game);
+      clearInterval(this.spawner);
+      this.game = window.setInterval(this.update, 20 * 5);
+      this.spawner = window.setInterval(this.balloonSpawner, 1000 * 5);
+    },
+    aimMove(e) {
+      this.aimPosition = e.target.getStage().getPointerPosition();
+      let x = this.aimPosition.x,
+        y = this.aimPosition.y,
+        radius = this.configScope.radius;
+      this.configScope.x = x;
+      this.configScope.y = y;
+      this.configCross[0].points = [x, y + radius, x, y - radius];
+      this.configCross[1].points = [x + radius, y, x - radius, y];
+    },
+    shoot() {
+      this.isAiming = false;
+      clearInterval(this.game);
+      clearInterval(this.spawner);
+      this.game = window.setInterval(this.update, 20);
+      this.spawner = window.setInterval(this.balloonSpawner, 1000);
+      for (let i = 0; i < this.configBalloon.length; ++i) {
+        if (
+          canvasTools.distance(this.aimPosition, this.configBalloon[i]) <=
+          this.configBalloon[i].radius
+        ) {
+          this.checkAnswer(this.configOptions[i].text);
+          this.configBalloon.splice(i, 1);
+          this.configOptions.splice(i, 1);
+          break;
+        }
+      }
+    },
+    checkAnswer(selectedOption) {
+      let checkAnswer = false;
+      for (let answer in this.GameData.True) {
+        if (this.GameData.True[answer] == selectedOption) checkAnswer = true;
+      }
+      if (checkAnswer) {
+        this.$emit("play-effect", "CorrectSound");
+        this.$emit("add-record", ["#", selectedOption, "正確"]);
+        this.allOptions = this.allOptions.filter(
+          (option) => option != selectedOption
+        );
+        this.trueOptions = this.trueOptions.filter(
+          (option) => option != selectedOption
+        );
+      } else {
+        this.$emit("play-effect", "WrongSound");
+        this.$emit("add-record", ["#", selectedOption, "錯誤"]);
+      }
+      if (this.trueOptions.length == 0) this.$emit("next-question");
     },
   },
 };
