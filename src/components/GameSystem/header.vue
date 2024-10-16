ee61c7fab3dbf36ebc0ce20c88c3a806bfb40f65
2min0623 <blackmaple.hsiao@gmail.com>
2024-10-16 16:00:10 +0800

    refactor: Extract header component into a separate file

---
+++ src/components/GameSystem/header.vue
@@ -0,0 +1,99 @@
+<template>
+    <header>
+        <nav class="container-fluid navbar navbar-expand-md navbar-light sticky-top justify-content-around justify-content-md-center"
+            style="justify-content: flex-start !important">
+            <a class="navbar-brand mx-3" href="#" alt="Home">
+                <img src="@/assets/images/nav_bar/logo.png" />
+            </a>
+            <button class="navbar-toggler btn btn-primary mx-auto" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarText11" aria-controls="navbarText11" aria-expanded="false"
+                aria-label="Toggle navigation" @click="PreviousPage">
+                上一頁
+            </button>
+            <div class="collapse navbar-collapse mx-3" id="navbarText">
+                <div class="container sticky-top d-flex justify-content-end" style="--bs-breadcrumb-divider: '>'">
+                    <ol class="breadcrumb mb-0">
+                        <li class="breadcrumb-item">
+                            <i class="bi bi-house"></i><a href="#"> 主頁</a>
+                        </li>
+                        <li class="breadcrumb-item" aria-current="page" @click="PreviousPage">
+                            <i class="bi bi-book-half"></i>
+                            <a>{{ grade }} 年級 {{ subject }}</a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            <i class="bi bi-pen"></i><a> {{ gameName }}</a>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        grade: {
+            type: String,
+        },
+        gameName: {
+            type: String,
+        },
+        subject: {
+            type: String,
+        }
+    },
+    methods: {
+        PreviousPage() {
+            this.$emit('previous-page');
+        },
+
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    background-color: #f19c79;
+    height: 10vh !important;
+    touch-action: none;
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+}
+
+.navbar {
+    background-color: #f19c79;
+
+    .navbar-brand {
+        img {
+            @media (max-width: 576px) {
+                height: 7vh;
+                width: auto;
+            }
+
+            @media (min-width: 576px) {
+                max-width: 20vw;
+            }
+        }
+    }
+
+    img {
+        max-width: 80%;
+    }
+}
+
+.breadcrumb .breadcrumb-item {
+  a {
+    color: #fff; /* 替換為你想要的顏色 */
+    font-size: 1.2em;
+  }
+}
+</style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,50 +1,10 @@
 <template>
   <div id="GameView" ref="GameView">
-    <header>
-      <nav
-        class="container-fluid navbar navbar-expand-md navbar-light sticky-top justify-content-around justify-content-md-center"
-        style="justify-content: flex-start !important"
-      >
-        <a class="navbar-brand mx-3" href="#" alt="Home">
-          <img src="@/assets/images/nav_bar/logo.png" />
-        </a>
-        <button
-          class="navbar-toggler btn btn-primary mx-auto"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarText11"
-          aria-controls="navbarText11"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          @click="PreviousPage"
-        >
-          上一頁
-        </button>
-        <div class="collapse navbar-collapse mx-3" id="navbarText">
-          <div
-            class="container sticky-top d-flex justify-content-end"
-            style="--bs-breadcrumb-divider: '>'"
-          >
-            <ol class="breadcrumb mb-0">
-              <li class="breadcrumb-item">
-                <i class="bi bi-house"></i><a href="#"> 主頁</a>
-              </li>
-              <li
-                class="breadcrumb-item"
-                aria-current="page"
-                @click="PreviousPage"
-              >
-                <i class="bi bi-book-half"></i>
-                <a>{{ this.Grade }} 年級 {{ Subjects[Subject] }}</a>
-              </li>
-              <li class="breadcrumb-item active" aria-current="page">
-                <i class="bi bi-pen"></i><a> {{ this.Name }}</a>
-              </li>
-            </ol>
-          </div>
-        </div>
-      </nav>
-    </header>
+    <Header
+      :grade="this.Grade"
+      :gameName="this.Name"
+      :subject="Subjects[Subject]"
+      @previous-page="PreviousPage" />
     <section>
       <div class="">
         <div class="Container">
@@ -373,6 +333,7 @@ import fetchJson from "@/utilitys/fetch-json.js";
 import * as Arr2CSV from "@/utilitys/array2csv.js";
 import loading from "@/components/loading.vue";
 import GameStartandOver from "@/components/GameSystem/GameStartandOver.vue";
+import Header from "@/components/GameSystem/header.vue";
 import scratchSheet from "@/components/ScratchSheets.vue";
 import hintbutton from "@/components/GameSystem/hintbutton.vue";
 import * as ImportUrl from "@/utilitys/get_assets.js";
@@ -926,6 +887,7 @@ export default {
     hintbutton,
     scratchSheet,
     GameStartandOver,
+    Header,
     loading,
     LinkGame: defineAsyncComponent(() =>
       import("@/views/GameTemplate/LinkGame.vue")
@@ -1000,32 +962,6 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-header {
-  background-color: #f19c79;
-  height: 10vh !important;
-  touch-action: none;
-  user-select: none;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-}
-.navbar {
-  background-color: #f19c79;
-  .navbar-brand {
-    img {
-      @media (max-width: 576px) {
-        height: 7vh;
-        width: auto;
-      }
-      @media (min-width: 576px) {
-        max-width: 20vw;
-      }
-    }
-  }
-  img {
-    max-width: 80%;
-  }
-}
 .img-hover-zoom {
   transition: transform 0.3s ease; /* 平滑的過渡效果 */
 }
@@ -1034,12 +970,6 @@ header {
   transform: scale(1.07); /* 放大至原大小的 110% */
 }
 
-.breadcrumb .breadcrumb-item {
-  a {
-    color: #fff; /* 替換為你想要的顏色 */
-    font-size: 1.2em;
-  }
-}
 .img-hover-zoom {
   max-width: 100%; /* 或者是卡片寬度的百分比 */
   height: auto; /* 保持圖片的比例 */
