4c177d650facfe71e2ab95bf22ea6a56a4d76696
2min0623 <blackmaple.hsiao@gmail.com>
2024-10-22 10:13:01 +0800

    refactor(modal): 將mediaModal重構為獨立組件

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -129,36 +129,14 @@
             </div>
           </div>
 
-          <div v-if="showModal" class="mediaModal-overlay" @click.self="closeMediaModal">
-            <div class="mediaModal-container">
-              <div class="mediaModal-header">
-                <h1 class="mediaModal-title">{{ modalTitle }}</h1>
-              </div>
-              <div class="mediaModal-body">
-                <p class="mediaModal-content">{{ modalContent }}</p>
-                <video
-                  v-if="mediaType === 'video'"
-                  :src="mediaSrc"
-                  controls
-                  autoplay
-                  class="media-content"
-                ></video>
-                <img
-                  v-else-if="mediaType === 'image'"
-                  :src="mediaSrc"
-                  class="media-content"
-                />
-                <img
-                  v-else
-                  src="@/assets/images/game_images/elephant.gif"
-                  class="media-content"
-                />
-              </div>
-              <div class="mediaModal-footer">
-                <button type="button" @click="closeMediaModal">我知道了!</button>
-              </div>
-            </div>
-          </div>
+          <MediaModal
+            :showMediaModal="showMediaModal"
+            :modalTitle="modalTitle"
+            :modalContent="modalContent"
+            :mediaSrc="mediaSrc"
+            :mediaType="mediaType"
+            @closeModal="closeMediaModal"
+          />
         </div>
       </div>
     </section>
@@ -172,6 +150,7 @@ import loading from "@/components/loading.vue";
 import GameStartandOver from "@/components/GameSystem/GameStartandOver.vue";
 import Header from "@/components/GameSystem/header.vue";
 import LevelAndTime from "@/components/GameSystem/LevelAndTime.vue";
+import MediaModal from '@/components/GameSystem/MediaModal.vue';
 import scratchSheet from "@/components/ScratchSheets.vue";
 import hintbutton from "@/components/GameSystem/hintbutton.vue";
 import * as ImportUrl from "@/utilitys/get_assets.js";
@@ -233,7 +212,7 @@ export default {
       questionOrder : [],
       questionCopy: [],
       isGif: false,
-      showModal:false,
+      showMediaModal:false,
       modalTitle:"",
       mediaType:"none",
       mediaSrc:"",
@@ -414,10 +393,10 @@ export default {
       }
     },
     openMediaModal() {
-      this.showModal = true;
+      this.showMediaModal = true;
     },
     closeMediaModal() {
-      this.showModal = false;
+      this.showMediaModal = false;
     },
     ChangeGameStatus(status) {
       //改變遊戲狀態
@@ -740,6 +719,7 @@ export default {
     GameStartandOver,
     Header,
     LevelAndTime,
+    MediaModal,
     loading,
     LinkGame: defineAsyncComponent(() =>
       import("@/views/GameTemplate/LinkGame.vue")
@@ -936,97 +916,6 @@ export default {
 .slide-right-enter-to,
 .slide-right-leave {
   transform: translateX(0%);
-}
-
-.mediaModal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 1000;
-}
-
-.mediaModal-container {
-  background: #fff;
-  border-radius: 10px;
-  width: 80%;
-  height: 90%;
-  max-width: 90vw;
-  max-height: 90vh;
-  overflow: auto;
-  padding: 20px;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  gap: 10px;
-}
-
-.mediaModal-header {
-  width: 100%;
-  text-align: center;
-  margin-bottom: 10px;
-}
-
-.mediaModal-title {
-  font-size: 2rem;
-  text-align: start;
-  margin: 0;
-}
-
-.mediaModal-body {
-  width: 100%;
-  height: 80%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  flex-grow: 1; 
-}
-
-.mediaModal-footer {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-}
-
-.media-content {
-  max-width: 100%;
-  max-height: 100%;
-  min-width: 300px;
-  min-height: 200px;
-  object-fit: contain;
-  flex-grow: 1;
-}
-
-button {
-  background-color: #6c757d;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  cursor: pointer;
-  border-radius: 5px;
-}
-
-button:hover {
-  background-color: #5a6268;
-}
-
-img.media-content {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-}
-
-.mediaModal-content {
-  margin: 0;
-  font-size: 1.5rem;
 }
 
 .scratchsheet-overlay {
